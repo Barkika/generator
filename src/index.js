0f6f8f376b53de94b6ff80d46f2849bb5aaa9177
Barkika <barca.kralikova@gmail.com>
2025-01-09 11:31:19 +0100

    Final user experience implementations

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ function generateQuote(event) {
   let apiKey = "b84306dfa4e854abf13actc00831o2ff";
   let context = `Generate a motivational quote about ${instructionsInput.value}`;
   let prompt =
-    "You are a motivational life guru and love to inspire people. Your life-goal is to generate quote about life to make a better day for someone. Add a jin-jang emoji after every qyoute you generate.";
+    "You are a motivational life guru and love to inspire people. Your life-goal is to generate quote about life to make a better day for someone. Add a emoji after every qyoute you generate in the theme of quote.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt='${prompt}&context=${context}&key=${apiKey}`;
+
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="generating">⏳Generating quote for you about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayQuote);
 }
