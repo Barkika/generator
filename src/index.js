cc6e0083834482bdf466bfc0c8f07ca30f97ac83
Barkika <barca.kralikova@gmail.com>
2025-01-09 10:11:39 +0100

    JS code implementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "“Don't let someone else's opinion of you become your reality.”",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
